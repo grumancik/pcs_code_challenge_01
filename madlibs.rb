b76e6d93f9d2aa1427279de6c314d19f7c16f7c9
grumancik <genrumancik@gmail.com>
2013-11-30 20:42:22 -0800

    Increment 2

--- madlibs.rb
+++ madlibs.rb
@@ -1,3 +1,23 @@
-puts "Please enter a noun or noun phrase"
-text = gets.chomp
-puts "The #{text} went to sea in a sturdy ship."
+# Increment 1
+
+# puts "Please enter a noun or noun phrase"
+# text = gets.chomp
+# puts "The #{text} went to sea in a sturdy ship."
+
+# Increment 2
+
+parts_of_speech = Hash.new
+
+puts "Please enter a noun"
+parts_of_speech[:noun] = gets.chomp
+puts "And an adjective?"
+parts_of_speech[:adjective] = gets.chomp
+puts "How about a verb?"
+parts_of_speech[:verb] = gets.chomp
+puts "Adverb?"
+parts_of_speech[:adverb] = gets.chomp
+puts "And finally, an object please"
+parts_of_speech[:object] = gets.chomp
+
+puts "The #{parts_of_speech[:adjective]} #{parts_of_speech[:noun]} suddenly and #{parts_of_speech[:adverb]} had to #{parts_of_speech[:verb]} the #{parts_of_speech[:object]}."
+
